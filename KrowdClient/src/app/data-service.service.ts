1eb3c6dc992a58c18b23d6486408256fb5e049d0
unknown <cbejar93@gmail.com>
2019-01-10 15:31:38 -0500

    another get req

--- KrowdClient/src/app/data-service.service.ts
+++ KrowdClient/src/app/data-service.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Event } from './home/event.model'
 import { UserModelService } from './user-model.service';
+import 'rxjs/Rx'; 
+import { Observable } from 'rxjs/Observable'; 
+import { catchError } from 'rxjs/operators'; 
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +14,29 @@ export class DataServiceService {
 
   constructor(private httpClient: HttpClient) { }
 
+  getAllEvents(){
+    return this.httpClient.get<Event []>("localhost:7001/Juntos-Krowd/users/all")
+      .map((events)=>{
+        let eventData = events;
+        return eventData;
+
+      })
+      .pipe(catchError(error=>{
+        return throwError(error);
+      }))
+  }
+
   getAllUsers (){
 
    return this.httpClient.get<UserModelService []>("localhost:7001/Juntos-Krowd/users/all")
-      // .map((events)=>{
+      .map(
+          (users)=>{
+            let usersData = users;
+            return usersData; 
 
-
-      // })
+      })
+      .pipe(catchError(error=>{
+        return throwError(error);
+      }))
   }
 }
