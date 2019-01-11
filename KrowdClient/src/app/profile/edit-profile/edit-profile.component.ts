dce61f17a9faff2ecab2653d0339a44e26d559c5
Kaleigh Jaeger <jaegerka@dukes.jmu.edu>
2019-01-11 11:52:01 -0500

    Made changes to edit profile component

--- KrowdClient/src/app/profile/edit-profile/edit-profile.component.ts
+++ KrowdClient/src/app/profile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CloudinaryOptions, CloudinaryUploader } from 'ng2-cloudinary';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
+
+  submitChanges(form: NgForm) {}
+
+  //code for image upload
+  selectedFile: File = null;
+  imageURL: string;
+  picture: string = "https://static.licdn.com/scds/common/u/images/themes/katy/ghosts/person/ghost_person_200x200_v1.png";
+  constructor(http: HttpClient) { }
+
+  uploader: CloudinaryUploader = new CloudinaryUploader(
+    new CloudinaryOptions({ cloudName: 'dhazivqjc', uploadPreset: 'zalhcbr6' })
+    );
+
+    loading: any;
+    upload(){
+      this.loading = true;
+      this.uploader.uploadAll();
+      this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any): any => {
+           let res: any = JSON.parse(response);
+           console.log(res);
+           this.imageURL=res.url;
+           console.log(this.imageURL);
+           this.picture=this.imageURL;
+       }
+       this.uploader.onErrorItem = function(fileItem, response, status, headers) {
+          console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        console.log("picture upload successful")
+    }
 
 }
